710568f68ca1792a4c382133205186c6b1aef10b
Eric <ericpoliveira@gmail.com>
2021-11-28 17:22:52 -0300

    calculo-imc

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso, altura, imc;
+            //início
+            Console.WriteLine("Digite o seu peso:");
+                peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua altura");
+                altura = double.Parse(Console.ReadLine());
+
+            //processamento
+
+            imc = peso / (altura * altura);
+
+            //saída
+
+            Console.WriteLine("O seu IMC é: " + imc);
+                Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
